5114777f5b64ed307170894c521676d8aa42dd40
beckluca1 <beckluca1@gmail.com>
2022-12-31 14:13:10 +0100

    First gnuplot

--- WiPro/WiPro.cpp
+++ WiPro/WiPro.cpp
@@ -1,20 +1,36 @@
-// WiPro.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
-//
-
-#include <iostream>
+#include "WiPro.h"
+#pragma warning(disable:4996)
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
+
+    int x = 0;
+    int y = 0;
+
+    FILE* gnupipe = NULL;
+    FILE* fp = NULL;
+
+    const char* gnucommands[] = {"set title \"Demos\"", "plot 'data.tmp'"};
+
+    fp = fopen("data.tmp", "w");
+
+    if (!fp) {
+        std::perror("File opening failed");
+        return 0;
+    }
+
+    gnupipe = _popen("gnuplot -persistent", "w");
+
+    for (int i = 0; i < 13; i++)
+    {
+        fprintf(fp, "%d %d\n", x, y);
+        x += 2;
+        y += 1;
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        fprintf(gnupipe, "%s\n", gnucommands[i]);
+    }
 }
-
-// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
-
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
